9fbd2e6d29b727c1d3d5f06ab7024a7137c6baf0
Adrian <144566998+Adrian-Augusto@users.noreply.github.com>
2025-05-18 18:29:03 -0300

    Controler configurado

--- src/main/java/br/com/alura/AluraFake/task/Controller/TaskController.java
+++ src/main/java/br/com/alura/AluraFake/task/Controller/TaskController.java
@@ -1,18 +1,34 @@
 package br.com.alura.AluraFake.task.Controller;
 
+import br.com.alura.AluraFake.task.dto.NewOpenTextDTO;
+import br.com.alura.AluraFake.task.entity.NewOpenText;
+import br.com.alura.AluraFake.task.entity.NewSingleChoice;
+import br.com.alura.AluraFake.task.services.NewOpenTextService;
+import br.com.alura.AluraFake.task.services.NewSingleChoiceService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TaskController {
 
+    private final   NewOpenTextService service;
+    private final   NewSingleChoiceService singleChoiceService;
+
+    public TaskController(NewOpenTextService service, NewSingleChoiceService singleChoiceService) {
+        this.service = service;
+        this.singleChoiceService = singleChoiceService;
+    }
+
     @PostMapping("/task/new/opentext")
-    public ResponseEntity newOpenTextExercise() {
+    public ResponseEntity<NewOpenText> newOpenTextExercise(@Valid @RequestBody NewOpenTextDTO dto) {
+        NewOpenText newOpenText = service.newOpenTextExercise(dto);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/task/new/singlechoice")
-    public ResponseEntity newSingleChoice() {
+    public ResponseEntity<NewSingleChoice>newSingleChoice(@Valid @RequestBody NewSingleChoiceDTO dto) {
+        NewSingleChoice newSingleChoice = singleChoiceService.singlechoice(dto);
         return ResponseEntity.ok().build();
     }
 
