0053622a140f9839cd935b056c570a088912fb74
Adrian <144566998+Adrian-Augusto@users.noreply.github.com>
2025-05-19 03:57:34 -0300

    Feat NewMultipleService Create

---
+++ src/main/java/br/com/alura/AluraFake/task/services/NewMultipleService.java
@@ -0,0 +1,36 @@
+package br.com.alura.AluraFake.task.services;
+
+import br.com.alura.AluraFake.course.Course;
+import br.com.alura.AluraFake.course.CourseRepository;
+import br.com.alura.AluraFake.task.MapperTask.NewMultipleChoiceMapper;
+import br.com.alura.AluraFake.task.dto.NewMultiplechoiceDTO;
+import br.com.alura.AluraFake.task.entity.NewMultiplechoice;
+import br.com.alura.AluraFake.task.repository.NewMultiplechoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NewMultipleService {
+
+    @Autowired
+    private NewMultiplechoiceRepository repository;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public NewMultiplechoice newMultipleChoice(NewMultiplechoiceDTO dto) {
+
+        // Busca o curso pelo id, ou lança erro se não achar
+        Course course = courseRepository.findById(dto.getCourseId())
+                .orElseThrow(() -> new RuntimeException("Curso não encontrado"));
+
+        // Mapeia o DTO para entidade
+        NewMultiplechoice entity = NewMultipleChoiceMapper.toEntity(dto);
+
+        // Atribui o curso na entidade
+        entity.setCourse(course);
+
+        // Salva e retorna
+        return repository.save(entity);
+    }
+}
