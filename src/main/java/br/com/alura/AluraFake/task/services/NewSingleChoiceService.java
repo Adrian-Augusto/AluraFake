b0d2a539bff8cd6e870dbd6d0bda96a6fc72e88a
Adrian <144566998+Adrian-Augusto@users.noreply.github.com>
2025-05-19 09:25:08 -0300

    Classe para validar NewSingleChoice services

--- src/main/java/br/com/alura/AluraFake/task/services/NewSingleChoiceService.java
+++ src/main/java/br/com/alura/AluraFake/task/services/NewSingleChoiceService.java
@@ -3,6 +3,7 @@ import br.com.alura.AluraFake.course.Course;
 
 import br.com.alura.AluraFake.course.CourseRepository;
 import br.com.alura.AluraFake.task.MapperTask.NewSingleChoiceMapper;
+import br.com.alura.AluraFake.task.Validation.NewSingleChoiceValidation;
 import br.com.alura.AluraFake.task.dto.NewSingleChoiceDTO;
 import br.com.alura.AluraFake.task.entity.NewSingleChoice;
 import br.com.alura.AluraFake.task.entity.Options;
@@ -14,28 +15,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class NewSingleChoiceService {
 
-    @Autowired private NewSingleChoiceRepository repository;
-    @Autowired private CourseRepository courseRepository;
 
+        @Autowired
+        private NewSingleChoiceRepository repository;
 
+        @Autowired
+        private CourseRepository courseRepository;
 
+        @Autowired
+        private NewSingleChoiceValidation validation;
 
-     public NewSingleChoice singlechoice (NewSingleChoiceDTO dto  ) {
+        public NewSingleChoice singlechoice(NewSingleChoiceDTO dto) {
 
+            // Busca o curso pelo id, ou lança erro se não achar
+            Course course = courseRepository.findById(dto.getCourseId())
+                    .orElseThrow(() -> new RuntimeException("Curso não encontrado"));
 
-        // Busca o curso pelo id, ou lança erro se não achar
-        Course course = courseRepository.findById(dto.getCourseId())
-          .orElseThrow(() -> new RuntimeException("Curso não encontrado"));
+            // Mapeia o DTO para entidade
+            NewSingleChoice entity = NewSingleChoiceMapper.toEntity(dto);
 
-         Options option = new Options();
-         option.setOption(option.getOption());
+            // Valida a entidade com o curso
+            validation.validateFull(entity, course);
 
-         // Mapeia o DTO para entidade
-        NewSingleChoice entity = NewSingleChoiceMapper.toEntity(dto );
+            // Atribui o curso na entidade
+            entity.setCourse(course);
 
-        // Atribui o curso na entidade
-        entity.setCourse(course);
-
-        // Salva e retorna
-        return repository.save(entity);
-    }}
+            // Salva e retorna
+            return repository.save(entity);
+        }
+    }
