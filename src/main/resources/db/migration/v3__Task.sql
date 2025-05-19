372d857d014235988ba5fb9cc8cd917073ec7f23
Adrian <144566998+Adrian-Augusto@users.noreply.github.com>
2025-05-18 23:30:20 -0300

    Tabalea task criada

---
+++ src/main/resources/db/migration/v3__Task.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE tasks
+(
+    id        BIGINT AUTO_INCREMENT NOT NULL,
+    dtype     VARCHAR(31)           NULL,
+    statement VARCHAR(255)          NULL,
+    orderr    INT                   NULL,
+    course_id BIGINT                NOT NULL,
+    type      VARCHAR(255)          NULL,
+    CONSTRAINT pk_tasks PRIMARY KEY (id)
+);
+
+ALTER TABLE tasks
+    ADD CONSTRAINT FK_TASKS_ON_COURSEID FOREIGN KEY (courseId) REFERENCES alurafake.Course (id);
