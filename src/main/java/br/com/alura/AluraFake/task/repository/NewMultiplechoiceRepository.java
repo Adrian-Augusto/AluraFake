cfcbd0b14e2da174b864502c1da3ba4c03f637bb
Adrian <144566998+Adrian-Augusto@users.noreply.github.com>
2025-05-20 09:52:45 -0300

    findByCourseIdOrderByOrderrAsc buscr em order Ascendent O curso

--- src/main/java/br/com/alura/AluraFake/task/repository/NewMultiplechoiceRepository.java
+++ src/main/java/br/com/alura/AluraFake/task/repository/NewMultiplechoiceRepository.java
@@ -2,6 +2,9 @@ package br.com.alura.AluraFake.task.repository;
 
 import br.com.alura.AluraFake.course.Course;
 import br.com.alura.AluraFake.task.entity.NewMultiplechoice;
+import br.com.alura.AluraFake.task.services.NewMultipleService;
+import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +17,7 @@ import java.util.List;
 public interface NewMultiplechoiceRepository extends JpaRepository<NewMultiplechoice, Long> {
 
     boolean existsByCourseAndStatement(Course course, String statement);
+
+
+    List<NewMultiplechoice> findByCourseIdOrderByOrderrAsc(Long courseId);
 }
