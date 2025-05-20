fe3c3b3b6fe5b5f584a964457d1bd65dd056d44b
Adrian <144566998+Adrian-Augusto@users.noreply.github.com>
2025-05-19 21:50:58 -0300

    Metodo para ordenar list

--- src/main/java/br/com/alura/AluraFake/task/repository/NewSingleChoiceRepository.java
+++ src/main/java/br/com/alura/AluraFake/task/repository/NewSingleChoiceRepository.java
@@ -2,11 +2,16 @@ package br.com.alura.AluraFake.task.repository;
 
 import br.com.alura.AluraFake.course.Course;
 import br.com.alura.AluraFake.task.entity.NewSingleChoice;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NewSingleChoiceRepository extends JpaRepository<NewSingleChoice, Long> {
 
     boolean existsByCourseAndStatement(Course course, String statement);
+
+    List<NewSingleChoice> findByCourseIdOrderByOrderrAsc(Long courseId);
 }
