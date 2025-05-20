d1a187720fcc28e5f1ef09a415042b3114e99bf2
Adrian <144566998+Adrian-Augusto@users.noreply.github.com>
2025-05-19 21:49:17 -0300

    Feat Repostiorio tabela task

--- src/main/java/br/com/alura/AluraFake/task/repository/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/task/repository/TaskRepository.java
@@ -1,8 +1,11 @@
 package br.com.alura.AluraFake.task.repository;
 
+import br.com.alura.AluraFake.task.Type.Type;
 import br.com.alura.AluraFake.task.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 
